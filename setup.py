9075c81d5db192d534025553b138a60844613d11
Jeff Coggshall <deeplearnjeff@gmail.com>
2024-07-12 16:44:40 -0700

    Update package directory to "finpak" instead of "src" in setup.py.

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open('requirements.txt') as f:
 setup(
     name="FINPAK",
     version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(where="finpak"),
+    package_dir={"": "finpak"},
     install_requires=requirements,
     entry_points={
         'console_scripts': [
