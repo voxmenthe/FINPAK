6b76a79a27cc5186f89eecfb240fc356677a557c
Jeff Coggshall <deeplearnjeff@gmail.com>
2024-07-12 17:44:25 -0700

    Added functions to check existing data in CSV and Parquet formats.

--- tests/finpak/data/fetchers/test_yahoo.py
+++ tests/finpak/data/fetchers/test_yahoo.py
@@ -1,17 +1,23 @@
 import unittest
 import pandas as pd
 
-from financial_analysis.data.fetchers.yahoo import (
+from finpak.data.fetchers.yahoo import (
     download_historical_data,
     download_multiple_tickers,
     save_data_to_file,
+    check_existing_data,
     update_data_file
 )
 
 
 class TestYahooFetchers(unittest.TestCase):
 
-    def test_download_historical_data(self):
+    def test_download_historical_data_with_existing_data(self):
+        data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
+        save_data_to_file(data, 'AAPL', 'csv')
+        existing_data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
+        self.assertIsInstance(existing_data, pd.DataFrame)
+        self.assertFalse(existing_data.empty)
         data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
         self.assertIsInstance(data, pd.DataFrame)
         self.assertFalse(data.empty)
@@ -25,11 +31,23 @@ class TestYahooFetchers(unittest.TestCase):
 
     def test_save_data_to_file_csv(self):
         data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
+        save_data_to_file(data, 'AAPL', 'csv')
+        folder_path = os.path.join('data_store', 'AAPL')
+        filename = os.path.join(folder_path, f"{date.today().strftime('%Y-%m-%d')}.csv")
+        loaded_data = pd.read_csv(filename, index_col=0, parse_dates=True)
+        pd.testing.assert_frame_equal(data, loaded_data)
+        data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
         save_data_to_file(data, 'test_data.csv', 'csv')
         loaded_data = pd.read_csv('test_data.csv', index_col=0, parse_dates=True)
         pd.testing.assert_frame_equal(data, loaded_data)
 
     def test_save_data_to_file_parquet(self):
+        data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
+        save_data_to_file(data, 'AAPL', 'parquet')
+        folder_path = os.path.join('data_store', 'AAPL')
+        filename = os.path.join(folder_path, f"{date.today().strftime('%Y-%m-%d')}.parquet")
+        loaded_data = pd.read_parquet(filename)
+        pd.testing.assert_frame_equal(data, loaded_data)
         data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
         save_data_to_file(data, 'test_data.parquet', 'parquet')
         loaded_data = pd.read_parquet('test_data.parquet')
@@ -37,12 +55,31 @@ class TestYahooFetchers(unittest.TestCase):
 
     def test_update_data_file_csv(self):
         data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
+        save_data_to_file(data, 'AAPL', 'csv')
+        update_data_file('AAPL', 'csv')
+        folder_path = os.path.join('data_store', 'AAPL')
+        filename = os.path.join(folder_path, f"{date.today().strftime('%Y-%m-%d')}.csv")
+        updated_data = pd.read_csv(filename, index_col=0, parse_dates=True)
+        self.assertFalse(updated_data.empty)
+        data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
         save_data_to_file(data, 'test_update_data.csv', 'csv')
         update_data_file('AAPL', 'test_update_data.csv', 'csv')
         updated_data = pd.read_csv('test_update_data.csv', index_col=0, parse_dates=True)
         self.assertFalse(updated_data.empty)
 
-    def test_update_data_file_parquet(self):
+    def test_check_existing_data_csv(self):
+        data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
+        save_data_to_file(data, 'AAPL', 'csv')
+        existing_data = check_existing_data('AAPL', '2020-01-01', '2020-01-10', '1d', 'csv')
+        self.assertIsInstance(existing_data, pd.DataFrame)
+        self.assertFalse(existing_data.empty)
+
+    def test_check_existing_data_parquet(self):
+        data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
+        save_data_to_file(data, 'AAPL', 'parquet')
+        existing_data = check_existing_data('AAPL', '2020-01-01', '2020-01-10', '1d', 'parquet')
+        self.assertIsInstance(existing_data, pd.DataFrame)
+        self.assertFalse(existing_data.empty)
         data = download_historical_data('AAPL', '2020-01-01', '2020-01-10')
         save_data_to_file(data, 'test_update_data.parquet', 'parquet')
         update_data_file('AAPL', 'test_update_data.parquet', 'parquet')
